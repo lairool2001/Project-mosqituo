372c78ca302c5fe82a2b483c29e3707e07caebc9
gcvi5633 <gcvi5633@gmail.com>
2015-10-28 16:34:59 +0800

    寫好了第一階段的倒數計時器，請針對秒數使用

---
+++ Assets/Script/Timecon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timecon : MonoBehaviour {
+    public float tc;                    //看是要幾秒  #Timecontroller
+    public Text time;
+    int min;
+    int sec;
+    // Use this for initialization
+    void Start() {
+
+            }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (tc > 0)
+            Timer();
+    }
+
+    public void Timer()
+    {
+        tc -= Time.deltaTime;
+        sec = (int)tc;
+        min = sec / 60;
+        sec = sec % 60;
+        if (sec % 60 < 10)
+            time.text = min.ToString() + ":0" + sec.ToString();
+        else
+            time.text = min.ToString() + ":" + sec.ToString();
+    }
+}
